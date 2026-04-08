1d95738ad5d191dcd5bfcc9df9d5a97d1f5711cd
Filipp <wolverinexdd@yandex.ru>
2026-04-08 21:04:00 +0400

    Lesson02-complicated

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(1223);
+const num = 266219;
+
+const numToString = num.toString().split("");
+
+const res = numToString.reduce((acc, num) => acc * num, 1) ** 3;
+
+console.log(res.toString().slice(0, 2));
+
