a04884f48a9a33fae823cf13ac3a567d1a8082d3
Filipp <wolverinexdd@yandex.ru>
2026-04-19 19:00:24 +0400

    lesson06

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const arr = ['2225', '52555', '333', '444', '777', '888', '4546616']
+const one = () => {
 
-const newArr = arr.filter((item) => {
-    return item.startsWith('2') || item.startsWith('4')
-})
+    let randomNumber = Math.floor(Math.random() * 100 + 1);
+    let attempt = 10;
 
-console.log(newArr);
+    console.log(randomNumber);
+    const two = () => {
+        if (attempt <= 0) {
+            const answer = confirm('"Попытки закончились, хотите сыграть еще')
+            if (answer) {
+                randomNumber = Math.floor(Math.random() * 10 + 1);
+                console.log(randomNumber);
+                attempt = 10;
+                two()
+            }
+            else return
 
-let filtered = []
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
-        console.log(arr[i]);
-        filtered.push(arr[i])
+        }
+        let a = prompt("Угадай число от 1 до 100")
+        if (a === null) {
+            alert('Игра окончена!')
+            return
+        }
+        const b = Number(a)
+
+        if (isNaN(b)) {
+            alert('Введи число!')
+            two()
+            return
+        }
+        if (b > randomNumber) {
+            attempt--
+            alert(`Загаданное число меньше, осталось попыток ${attempt}`)
+            two()
+        }
+        else if (b < randomNumber) {
+            attempt--
+            alert(`Загаданное число больше, осталось попыток ${attempt}`)
+            two()
+        }
+        else {
+            let newGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+            if (newGame) {
+
+                randomNumber = Math.floor(Math.random() * 10 + 1);
+                console.log(randomNumber);
+                attempt = 10;
+                two()
+            }
+        }
     }
+
+    two()
 }
 
-console.log(filtered);
+
+
+one()
+
+
 
